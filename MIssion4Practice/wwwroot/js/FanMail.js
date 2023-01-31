e979205f8659f54f4fabe9e85ee1feb1eec22f68
Darby Mecham <dloumech@byu.edu>
2023-01-30 22:10:07 -0700

    Add project files.

---
+++ MIssion4Practice/wwwroot/js/FanMail.js
@@ -0,0 +1,5 @@
+﻿document.getElementById("buttonSend").addEventListener("click", function () {
+    alert("From: " + document.getElementById("txtFrom").value +
+        ", Subject: " + document.getElementById("txtSubject").value +
+        ", Message: " + document.getElementById("txtMessage").value);
+})
